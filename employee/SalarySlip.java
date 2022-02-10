4ff7d98823ee1ebdab508b04e2cabb567a33967b
RaviJangir7X <98867720+RaviJangir7X@users.noreply.github.com>
2022-02-10 08:37:04 +0530

    Update SalarySlip.java

--- employee/SalarySlip.java
+++ employee/SalarySlip.java
@@ -1,10 +1,18 @@
 package employee;
 import xml.DatabaseManager;
 public class SalarySlip {
-        public static void printSalary(Employee employee) {
-            System.out.println("Name "+employee.getFirstName()+" "+employee.getLastName());
-            //System.out.println("Salary "+employee.getSalary());
+        public static void printSalary(List<Employee> employee) {
+                for (Employee e: employee) {
+                    System.out.println("******************");
+                    System.out.println("Salary Slip: ");
+                    System.out.println("Employee ID: " + e.getEmployeeId());
+                    System.out.println("Name: " + e.getFirstName() + " " + e.getLastName() );
+                    System.out.println("Email: " + e.getEmail() );
+                    System.out.println("Salary: " + e.getBaseSalary());
+                    System.out.println("*******************");
 
+                   
+                }
         }
 
     }
